a672f4610c9a89fd74a3f8d71776542db44092e1
pawann-2000 <github.3ljwu@ggmaill.aleeas.com>
2024-01-16 00:19:51 +0530

    fix

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -2,8 +2,7 @@ package com.driver;
 
 public class Main {
     public static void main(String[] args) {
-        B obj = new B();
-        System.out.println(obj.meth());
+        System.out.println(B.meth());
     }
 }
 class A {
