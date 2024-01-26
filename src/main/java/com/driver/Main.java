c5695cc444e5a28002113b3a3ff66a469a16d9c3
lion <github.3ljwu@ggmaill.aleeas.com>
2024-01-26 06:24:29 +0530

    Fixed

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,17 @@
 package com.driver;
 
+class A {
+    public static String meth(){
+        return "Invoking method from class A";
+    }
+}
+
+class B extends A {
+    public static String meth() {
+        return "Method is overridden in Extended class B";
+    }
+}
+
 public class Main {
     public static void main(String[] args) {
         System.out.println(B.meth());
