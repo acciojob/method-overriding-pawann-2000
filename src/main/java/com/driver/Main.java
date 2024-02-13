aa595a4382daafec7a477a25a704eee1b4297038
pawann-2000 <github.3ljwu@ggmaill.aleeas.com>
2024-02-13 21:25:30 +0530

    minor fix

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,12 +1,5 @@
 package com.driver;
 
-class Main{
-    public static void main(String[] args) {
-        B obj = new B();
-        System.out.println(obj.meth());
-    }
-}
-
 class A{
     public String meth(){
         return "Invoking method from class A";
@@ -16,5 +9,12 @@ class A{
 class B extends A{
     public String meth(){
         return "Method is overridden in Extended class B";
+    }
+}
+
+public class Main{
+    public static void main(String[] args) {
+        B obj = new B();
+        obj.meth();
     }
 }
