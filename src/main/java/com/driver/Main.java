4b359a537d1691acd44a0d0ff5a90194a9c48ee7
pawann-2000 <github.3ljwu@ggmaill.aleeas.com>
2024-02-13 20:55:05 +0530

    minor fix

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,17 +1,5 @@
 package com.driver;
 
-class A {
-    String meth() {
-        return "Invoking method from class A";
-    }
-}
-class B extends A {
-    @Override
-    String meth() {
-        return "Method is overridden in Extended class B";
-    }
-}
-
 public class Main {
     public static void main(String[] args) {
         B objB = new B();
