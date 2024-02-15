34417443a23580a03fa9247d57fcbc86acc3247e
pawann-2000 <github.3ljwu@ggmaill.aleeas.com>
2024-02-15 08:32:10 +0530

    minor fix

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -15,6 +15,6 @@ class B extends A{
 public class Main{
     public static void main(String[] args) {
         B obj = new B();
-        obj.meth();
+        System.out.println(obj.meth());
     }
 }
